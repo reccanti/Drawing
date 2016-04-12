476efa1fc112ddeb2de82b82d8db572ac91138a7
Ben Wilcox <reccanti@gmail.com>
2016-04-12 00:33:39 -0400

    created a controller for the model

--- server/controllers/Drawing.js
+++ server/controllers/Drawing.js
@@ -1,40 +1,54 @@
-// var models = require('../models');
-// var Drawing = models.Drawing;
+var models = require('../models');
+var Drawing = models.Drawing;
 
 
-// /**
-//  * Save an image to the database
-//  */
-// var saveImage = function (req, res) {
-//     if (!req.body.owner ||
-//         !req.body.dataURI) {
-//         return res.status(400).json({ error: 'All fields are required' });
-//     };
-//     var data = {
-//         owner: req.body.owner,
-//         image: req.body.dataURI,
-//     };
-//     // User.Model.generateHash(req.body.password, function (salt, hash) {
-//     //     var userData = {
-//     //         username: req.body.username,
-//     //         name: req.body.name,
-//     //         salt: salt,
-//     //         password: hash,
-//     //     };
-//     //     var newUser = new User.Model(userData);
-//     //     newUser.save(function (err) {
-//     //         if (err) {
-//     //             /* eslint no-console: 0 */
-//     //             console.error(err);
-//     //             return res.status(400).json({
-//     //                 error: 'an error occurred when saving your account to the database',
-//     //             });
-//     //         }
-//     //         req.session.account = newUser.toAPI();
-//     //         res.status(200).json(newUser.toAPI());
-//     //     });
-//     // });
-// };
+/**
+ * Save an image to the database
+ */
+/* eslint consistent-return: 0 */
+var saveImage = function (req, res) {
+    var saveData;
+    var drawing;
+    if (!req.body.owner ||
+        !req.body.dataURI) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+    saveData = {
+        owner: req.body.owner,
+        image: req.body.dataURI,
+    };
+    drawing = new Drawing.Model(saveData);
+    drawing.save(function (err) {
+        if (err) {
+             /* eslint no-console: 0 */
+            console.error(err);
+            return res.status(400).json({
+                error: 'an error occurred when saving your drawing to the database',
+            });
+        }
+        return res.status(200);
+    });
+    // User.Model.generateHash(req.body.password, function (salt, hash) {
+    //     var userData = {
+    //         username: req.body.username,
+    //         name: req.body.name,
+    //         salt: salt,
+    //         password: hash,
+    //     };
+    //     var newUser = new User.Model(userData);
+    //     newUser.save(function (err) {
+    //         if (err) {
+    //             /* eslint no-console: 0 */
+    //             console.error(err);
+    //             return res.status(400).json({
+    //                 error: 'an error occurred when saving your account to the database',
+    //             });
+    //         }
+    //         req.session.account = newUser.toAPI();
+    //         res.status(200).json(newUser.toAPI());
+    //     });
+    // });
+};
 
 
-// module.exports
+module.exports.saveImage = saveImage;
