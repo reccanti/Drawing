20d220d2068b2b11b2718901618b72fedbbe245e
Ben Wilcox <reccanti@gmail.com>
2016-05-01 02:00:46 -0400

    Fixed Gulp Task
    Fixed the materialize gulp task to send materialize to the front-end

--- dev/tasks/materialize.js
+++ dev/tasks/materialize.js
@@ -1,16 +1,23 @@
 var gulp = require('gulp');
 var merge = require('merge-stream');
+var path = require('path');
 
 var pipeMaterialize = function () {
-    var materializeStream =
-        gulp.src('../../src/js/vendors/materialize/materialize.min.js')
-        .pipe(gulp.dest('../../dist/js/'));
+    // var materializeStream =
+    //     gulp.src('../../src/js/vendors/materialize/materialize.min.js')
+    //     .pipe(gulp.dest('../../dist/js/'));
         
-    var jqueryStream =
-        gulp.src(require.resolve('jquery'))
-        .pipe(gulp.dest('../../dist/js'));
+    // var jqueryStream =
+    //     gulp.src(require.resolve('jquery'))
+    //     .pipe(gulp.dest('../../dist/js'));
         
-    return merge(materializeStream, jqueryStream);
+    // return merge(materializeStream, jqueryStream);
+    gulp.src(path.resolve(__dirname, '../../src/js/vendors/materialize/materialize.min.js'))
+    .pipe(gulp.dest(path.resolve(__dirname, '../../dist/js/')));
+    
+    
+    gulp.src(path.resolve(__dirname, require.resolve('jquery')))
+    .pipe(gulp.dest(path.resolve(__dirname, '../../dist/js')));
 }
 
 module.exports = pipeMaterialize;
