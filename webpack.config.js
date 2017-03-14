311a287a8f7153aa9b18d8affed95b71bfb2c07c
Ben Wilcox <reccanti@gmail.com>
2017-03-14 00:28:25 -0400

    Compile Sass and Fonts
    Compiles the Sass files and font files into a separate bundle.
    This puts the whole build process in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+// this will extract Sass files into a css file
+const extractSass = new ExtractTextPlugin({
+    filename: '[name].css',
+});
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/js/entry.jsx'),
+    entry: {
+        code: path.resolve(__dirname, 'src/js/entry.jsx'),
+        styles: path.resolve(__dirname, 'src/sass/main.scss')
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js'
     },
     devtool: 'source-map',
     module: {
@@ -13,8 +22,24 @@ module.exports = {
                 test: /\.(js|jsx)$/, 
                 use: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                        { loader: 'css-loader' },
+                        { loader: 'sass-loader' }
+                    ]
+                })
+            },
+            {
+                test: /\.(eot|woff|woff2|svg|ttf)$/,
+                use: 'file-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        extractSass
+    ]
 }
 
